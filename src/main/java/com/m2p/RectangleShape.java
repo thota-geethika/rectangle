64a9e81b315b8b63ee2a2b0b62611a10d33feb48
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-29 16:00:30 +0530

    Requirement 1: Add. Added the required testcases and logic to find the area of the Rectangle.

--- src/main/java/com/m2p/RectangleShape.java
+++ src/main/java/com/m2p/RectangleShape.java
@@ -3,4 +3,12 @@ package com.m2p;
 public class RectangleShape {
 
 
+    public double findArea(double length, double breadth) {
+
+        if(length < 0 || breadth <0)
+        {
+            throw new IllegalArgumentException();
+        }
+        return length*breadth;
+    }
 }
--- src/test/java/com/m2p/RectangleShapeTest.java
+++ src/test/java/com/m2p/RectangleShapeTest.java
@@ -1,8 +1,33 @@
 package com.m2p;
 
-import java.awt.*;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RectangleShapeTest {
 
     RectangleShape rectangleObject = new RectangleShape();
+
+    @Nested
+    class Area {
+        @Test
+        void toReturnAreaAsSixTeenWhenEightAndTwoAreSides() {
+            //Arrange
+            double expectedArea = 16;
+
+            //Act
+            double actualArea = rectangleObject.findArea(2, 8);
+
+            //Assert
+            assertEquals(expectedArea, actualArea);
+        }
+
+        @Test
+        void toReturnExceptionWhenNegativeValueEntered()
+        {
+            assertThrows(IllegalArgumentException.class, ()-> rectangleObject.findArea(-3,4));
+        }
+    }
 }
